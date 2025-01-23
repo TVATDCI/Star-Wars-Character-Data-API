807bfcbd933b5cb2962bd1afc0fd26982deaa04f
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2025-01-24 00:29:13 +0100

    backend implemented🚀

--- backend/controllers/characterController.js
+++ backend/controllers/characterController.js
@@ -1,13 +1,35 @@
 // Description: Controller for the character routes
 import Character from "../models/characterModel.js";
 
+// Reformat the character data to return how we want it to be displayed
+const formatCharacter = (character) => {
+  return {
+    name: character.name,
+    _id: character._id,
+    species: character.species,
+    homeworld: character.homeworld,
+    affiliation: character.affiliation,
+    stats: character.stats,
+    weapons: character.weapons,
+    vehicles: character.vehicles,
+    isJedi: character.isJedi,
+    apprentices: character.apprentices,
+    master: character.master,
+    notableAchievements: character.notableAchievements,
+    createdAt: character.createdAt,
+    updatedAt: character.updatedAt,
+    __v: character.__v,
+  };
+};
+
 // @desc    Get all characters
 // @route   GET /api/characters
 // @access  Public
 const getCharacters = async (req, res) => {
   try {
     const characters = await Character.find();
-    res.json(characters);
+    const formatCharacters = characters.map(formatCharacter);
+    res.json(formatCharacters);
   } catch (error) {
     res.status(500).json({ message: error.message });
   }
@@ -20,7 +42,7 @@ const getCharacterById = async (req, res) => {
   try {
     const character = await Character.findById(req.params.id);
     if (character) {
-      res.json(character);
+      res.json(formatCharacter(character));
     } else {
       res.status(404).json({ message: "Character not found" });
     }
@@ -34,9 +56,14 @@ const getCharacterById = async (req, res) => {
 // @access  Public
 const createCharacter = async (req, res) => {
   const character = new Character(req.body);
+  // logging just name & id of the character
+  console.log("Creating a new character:", {
+    name: character.name,
+    _id: character._id,
+  });
   try {
     const newCharacter = await character.save();
-    res.status(201).json(newCharacter);
+    res.status(201).json(formatCharacter(newCharacter));
   } catch (error) {
     res.status(400).json({ message: error.message });
   }
@@ -65,9 +92,10 @@ const updateCharacter = async (req, res) => {
 // @access  Public
 const deleteCharacter = async (req, res) => {
   try {
+    console.log("Deleting a character with an id: ", req.params.id);
     const character = await Character.findById(req.params.id);
     if (character) {
-      await character.remove();
+      await Character.deleteOne({ _id: req.params.id }); // Correct method to delete
       res.json({ message: "Character removed" });
     } else {
       res.status(404).json({ message: "Character not found" });
