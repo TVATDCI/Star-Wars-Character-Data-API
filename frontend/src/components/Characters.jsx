3e005374ccef0f244fbd6ac7a9c3a41de92f7f20
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2025-01-26 21:35:41 +0100

    implement fetching logic Characters.jsx

---
+++ frontend/src/components/Characters.jsx
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+
+function Characters({ onSelectCharacter }) {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/api/characters")
+      .then((response) => response.json())
+      .then((data) => setCharacters(data))
+      .catch((error) => console.error("Error fetching characters:", error));
+  }, []);
+
+  return (
+    <div className="bg-neutral-800/20 backdrop-blur-sm p-8 rounded-lg shadow-lg max-w-xs">
+      <h2 className="text-2xl text-red-600 font-bold mb-4">Characters</h2>
+      <ul>
+        {characters.map((character) => (
+          <li
+            key={character._id}
+            className="mb-2 text-neutral-200 hover:text-red-500 transition-colors duration-800 cursor-pointer"
+            onClick={() => onSelectCharacter(character._id)}
+          >
+            {character.name}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
+
+export default Characters;
