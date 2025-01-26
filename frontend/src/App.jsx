2dddff378b016ce237dc725172de52d9ab8af68b
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2025-01-26 21:30:43 +0100

    implement handleSelectCharacter

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,29 +1,61 @@
 import { useState, useEffect } from "react";
 import "./App.css";
 
+import Characters from "./components/Characters";
+import CharacterDetail from "./components/CharacterDetail";
+
 function App() {
   const [background, setBackground] = useState("");
+  const [selectedCharacterId, setSelectedCharacterId] = useState(null);
+  const [view, setView] = useState("info");
   useEffect(() => {
-    const imgUrl =
-      "https://images.unsplash.com/photo-1647071263966-ba986d17863a?q=80&w=2147&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+    const imgUrl = "https://www.transparenttextures.com/patterns/stardust.png";
     setBackground(imgUrl);
   }, []);
+
+  // Handle Character Selection - Once the view changes to characterDetail.jsx, the selected character's id is passed as a prop
+  const handleSelectCharacter = (id) => {
+    setSelectedCharacterId(id);
+    setView("characterDetail");
+  };
+
+  // Handle Back Button
+  const handleBack = () => {
+    setView("characters");
+  };
 
   return (
     <div
       className="h-screen flex items-center justify-center"
       style={{ backgroundImage: `url(${background})`, backgroundSize: "cover" }}
     >
-      <div className="bg-neutral-900/10 backdrop-blur-sm p-8 rounded-lg shadow-lg max-w-xs">
-        <h1 className="text-3xl text-red-600 font-bold mb-2 ">
-          Star Wars Character Database API
-        </h1>
-        <p className="text-lg gradient-text">
-          Create a REST API that manages a database of Star Wars characters.
-          Users should be able to create, read, update, and delete character
-          information through various endpoints.
-        </p>
-      </div>
+      {view === "info" && (
+        <div className="bg-neutral-800/20 backdrop-blur-sm p-8 rounded-lg shadow-lg max-w-xs">
+          <h1 className="text-3xl text-red-600 font-bold mb-2 ">
+            Star Wars Character Database API
+          </h1>
+          <p className="text-lg gradient-text">
+            Create a REST API that manages a database of Star Wars characters.
+            Users should be able to create, read, update, and delete character
+            information through various endpoints.
+          </p>
+          <a
+            className="text-blue-500 hover:text-cyan-900 transition-colors duration-800 cursor-pointer"
+            onClick={() => setView("characters")}
+          >
+            Characters
+          </a>
+        </div>
+      )}
+      {view === "characters" && (
+        <Characters onSelectCharacter={handleSelectCharacter} />
+      )}
+      {view === "characterDetail" && (
+        <CharacterDetail
+          characterId={selectedCharacterId}
+          onBack={handleBack}
+        />
+      )}
     </div>
   );
 }
