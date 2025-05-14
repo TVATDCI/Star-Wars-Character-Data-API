44ed79b1ffb980ce8d7a2cd042016432978b09d3
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2025-05-14 03:30:40 +0200

    NebulaCanvas before implement useRef

--- frontend/src/components/NebulaCanvas.jsx
+++ frontend/src/components/NebulaCanvas.jsx
@@ -1,6 +1,5 @@
 import { useEffect } from "react";
 import styled from "styled-components";
-import Meteor from "./Meteor";
 
 // Nebula canvas
 const StyledCanvas = styled.canvas`
@@ -11,16 +10,6 @@ const StyledCanvas = styled.canvas`
   height: 100%;
   z-index: 0;
 `;
-
-// Meteor Wrapper
-//const MeteorLayer = styled.div`
-//  position: absolute;
-//  top: 0;
-//  left: 0;
-//  width: 100%;
-//  height: 100%;
-//  z-index: 1;
-//`;
 
 function NebulaCanvas() {
   useEffect(() => {
