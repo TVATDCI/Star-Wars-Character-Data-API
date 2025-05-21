ae386020f67332f72984471008630ccfbb7c21d5
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2025-05-21 19:58:45 +0200

    create form/TextInput, NumberInput.jsx

---
+++ frontend/src/components/form/NumberInput.jsx
@@ -0,0 +1,28 @@
+import PropTypes from "prop-types";
+
+export default function NumberInput({ label, name, value, onChange }) {
+  return (
+    <div className="mb-2">
+      {label && (
+        <label className="block text-left mb-1 text-sm font-semibold text-white">
+          {label}
+        </label>
+      )}
+      <input
+        type="number"
+        name={name}
+        value={value}
+        onChange={onChange}
+        placeholder={label}
+        className="p-2 w-full"
+      />
+    </div>
+  );
+}
+
+NumberInput.propTypes = {
+  label: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  value: PropTypes.oneOfType([PropTypes.string, PropTypes.number]).isRequired,
+  onChange: PropTypes.func.isRequired,
+};
