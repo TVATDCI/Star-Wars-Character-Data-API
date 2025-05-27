bace05e5120c930645a27011ea7a48d6f46835f1
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2025-05-27 22:30:19 +0200

    create loginUser logic in utils/api.js, refactore LoginForm.jsx

--- frontend/src/components/utils/api.js
+++ frontend/src/components/utils/api.js
@@ -3,7 +3,7 @@
 // Check if the environment variable is set
 //console.log("API_BASE_URL:", API_BASE_URL); // Debugging log
 
-// utils/api.js
+// utils/api.js - RegisterUser
 export async function registerUser(email, password) {
   const response = await fetch("http://localhost:5000/register", {
     method: "POST",
@@ -17,6 +17,22 @@ export async function registerUser(email, password) {
   }
 
   return response.json(); // only call .json() on OK response
+}
+
+// utils/api.js - LoginUser
+export async function loginUser(email, password) {
+  const response = await fetch("http://localhost:5000/login", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify({ email, password }),
+  });
+
+  if (!response.ok) {
+    const errorData = await response.json().catch(() => ({})); // Prevent crash if empty body
+    throw new Error(errorData.error || "Login failed");
+  }
+
+  return response.json(); // return token and user role
 }
 
 // The register function is refactored to keep the code DRY
