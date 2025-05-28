279a1131126415a6585c3b488f71e385e90ca7cf
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2025-05-28 02:46:04 +0200

    handle-view-router

---
+++ frontend/src/components/ViewRouter.jsx
@@ -0,0 +1,99 @@
+import Characters from "./Characters";
+import CharacterDetail from "./CharacterDetail";
+import CharactersForm from "./CharactersForm";
+import LoginForm from "./reg-auth/LoginForm";
+import RegisterForm from "./reg-auth/RegisterForm";
+import InfoPage from "./views/InfoPage";
+import { useApp } from "../context/AppContext";
+
+export default function ViewRouter() {
+  const {
+    view,
+    setView,
+    selectedCharacterId,
+    setSelectedCharacterId,
+    setUser,
+  } = useApp();
+
+  const handleSelectCharacter = (id) => {
+    setSelectedCharacterId(id);
+    setView("characterDetail");
+  };
+
+  const handleBack = () => {
+    setView("characters");
+  };
+
+  const handleReturnToInfo = () => {
+    setView("info");
+  };
+
+  const handleAddCharacter = () => {
+    setSelectedCharacterId(null);
+    setView("charactersForm");
+  };
+
+  const handleEditCharacter = (id) => {
+    setSelectedCharacterId(id);
+    setView("charactersForm");
+  };
+
+  const handleSaveCharacter = () => {
+    setView("characters");
+  };
+
+  const handleCancel = () => {
+    setView("characters");
+  };
+
+  const handleLogin = (user) => {
+    setUser(user);
+    setView("info");
+  };
+
+  const handleRegister = () => {
+    setView("login");
+  };
+
+  switch (view) {
+    case "info":
+      return <InfoPage onRegister={handleRegister} />;
+
+    case "characters":
+      return (
+        <Characters
+          onSelectCharacter={handleSelectCharacter}
+          onAddCharacter={handleAddCharacter}
+          onClick={handleReturnToInfo}
+        />
+      );
+
+    case "characterDetail":
+      return (
+        <CharacterDetail
+          characterId={selectedCharacterId}
+          onBack={handleBack}
+          onEdit={handleEditCharacter}
+        />
+      );
+
+    case "charactersForm":
+      return (
+        <CharactersForm
+          characterId={selectedCharacterId}
+          onSave={handleSaveCharacter}
+          onCancel={handleCancel}
+          onBack={handleBack}
+        />
+      );
+
+    case "login":
+      return <LoginForm onLogin={handleLogin} />;
+
+    case "register":
+      return <RegisterForm onRegister={handleRegister} />;
+
+    default:
+      return <InfoPage />;
+  }
+}
