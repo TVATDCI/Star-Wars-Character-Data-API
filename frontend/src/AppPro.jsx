b21e2e4ffcd0ff88a1476b6795bc4c7e54c08479
TVATDCI <tuanthong.vaidyanond@dci-student.org>
2025-05-28 03:10:12 +0200

    reversed AppProvider

---
+++ frontend/src/AppPro.jsx
@@ -0,0 +1,44 @@
+import NebulaCanvas from "./components/spaceAtmos/NebulaCanvas";
+import starWarsNeonLogo from "./assets/star-wars-neon.svg";
+import ViewRouter from "./components/ViewRouter";
+import { AppProvider, useApp } from "./context/AppContext";
+import "./App.css";
+
+function Layout() {
+  const { background } = useApp();
+
+  return (
+    <>
+      <NebulaCanvas className="z-0" />
+      <div
+        className="min-h-screen flex flex-col items-center justify-center bg-cover mt-10"
+        style={{
+          backgroundImage: `url(${background})`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          opacity: 0.8,
+        }}
+      >
+        <header className="flex flex-col items-center text-center py-6 z-30">
+          <img
+            src={starWarsNeonLogo}
+            alt="Star Wars Logo"
+            className="mb-4 w-48 sm:w-64 md:w-80 lg:w-[28rem]"
+          />
+          <h2 className="text-2xl text-neutral-800/90 font-bold mt-2">
+            Character Database API
+          </h2>
+        </header>
+        <ViewRouter />
+      </div>
+    </>
+  );
+}
+
+export default function App() {
+  return (
+    <AppProvider>
+      <Layout />
+    </AppProvider>
+  );
+}
