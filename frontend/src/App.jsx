4a70e20c1f37ca8416a9649e55692bf573658d55
TVATDCI <tvatdci@gmail.com>
2026-01-29 21:59:30 +0100

    update:

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,32 +1,33 @@
-import NebulaCanvas from "./components/spaceAtmos/NebulaCanvas";
-import starWarsNeonLogo from "./assets/star-wars-neon.svg";
-import ViewRouter from "./components/ViewRouter";
-import { AppProvider, useApp } from "./context/AppContext";
-import "./App.css";
+import React from 'react';
+import NebulaCanvas from './components/spaceAtmos/NebulaCanvas';
+import starWarsNeonLogo from './assets/star-wars-neon.svg';
+import ViewRouter from './components/ViewRouter';
+import { AppProvider, useApp } from './context/AppContext';
+import './App.css';
 
 function Layout() {
   const { background } = useApp();
 
   return (
     <>
-      <NebulaCanvas className="z-0" />
+      <NebulaCanvas className='z-0' />
       <div
-        className="min-h-screen flex flex-col items-center justify-center bg-cover mt-10"
+        className='min-h-screen flex flex-col items-center justify-center bg-cover mt-10'
         style={{
           backgroundImage: `url(${background})`,
-          backgroundSize: "cover",
-          backgroundPosition: "center",
+          backgroundSize: 'cover',
+          backgroundPosition: 'center',
           opacity: 0.8,
           zIndex: 0.7,
         }}
       >
-        <header className="flex flex-col items-center text-center py-6 z-30">
+        <header className='flex flex-col items-center text-center py-6 z-30'>
           <img
             src={starWarsNeonLogo}
-            alt="Star Wars Logo"
-            className="mb-4 w-48 sm:w-64 md:w-80 lg:w-[28rem]"
+            alt='Star Wars Logo'
+            className='mb-4 w-48 sm:w-64 md:w-80 lg:w-md'
           />
-          <h2 className="text-2xl text-neutral-100/30 font-bold mt-2">
+          <h2 className='text-2xl text-neutral-100/30 font-bold mt-2'>
             Character Database API
           </h2>
         </header>
