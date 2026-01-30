fe9fd01a5bd38a1eea42e3a667cac0b7b3f5ea75
TVATDCI <tvatdci@gmail.com>
2026-01-31 00:56:58 +0100

    split server to server.js, add helmet limiter

--- backend/app.js
+++ backend/app.js
@@ -1,39 +1,18 @@
-import path from 'path';
-import { fileURLToPath } from 'url';
 import express from 'express';
-import dotenv from 'dotenv';
 import cors from 'cors';
-import jwt from 'jsonwebtoken';
+import helmet from 'helmet';
+import rateLimit from 'express-rate-limit';
 
-import connectDB from './config/db.js';
 import characterRoutes from './routes/characterRoutes.js';
 import authenticateToken from './middleware/authMiddleware.js';
 import publicRoutes from './routes/publicRoutes.js';
 import userProfileRoutes from './routes/userProfile.js';
-import User from './models/userModel.js';
-
-// Setup environment
-const env = process.env.NODE_ENV || 'development';
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-// 1. .env.development: is being used in development environment.
-dotenv.config({
-  path: path.resolve(__dirname, `../.env.${env}`),
-});
-
-// 2. Fallback: Load the base .env file if the variables weren't found
-dotenv.config({
-  path: path.resolve(__dirname, '../.env'),
-});
-// DEBUG: Log loaded environment variables in development
-console.log('🔍 Environment:', env);
-console.log('🔑 MONGO_URL Loaded:', process.env.MONGO_URL ? '✅ Yes' : '❌ No');
-
-// Connect to DB
-connectDB();
 
 const app = express();
+
+// Set security HTTP headers
+app.use(helmet());
+
 app.use(express.json());
 
 // Enable CORS
@@ -58,62 +37,17 @@ app.use(
   })
 );
 
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!'
+});
+app.use('/api', limiter);
+
+
 // Profile routes
 app.use('/profile', userProfileRoutes);
-console.log('User profile routes initialized');
-
-// Register route
-app.post('/register', async (req, res) => {
-  const { email, password, role } = req.body;
-
-  if (!email || !password) {
-    console.log('Email or password is missing');
-    return res.status(400).json({ error: 'Email and password are required' });
-  }
-
-  try {
-    const newUser = new User({
-      email,
-      password,
-      role: role || 'user',
-    });
-
-    await newUser.save();
-    console.log('User registered successfully');
-    res.status(201).json({ message: 'User registered successfully' });
-  } catch (error) {
-    console.error('Error registering user:', error);
-    res.status(400).json({ error: 'Email already exists' });
-  }
-});
-
-// Login route
-app.post('/login', async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email, password });
-
-  console.log('Login attempt for user:', email);
-  console.log('Logging in user HIT');
-  console.log('Request body:', req.body);
-
-  if (user) {
-    const token = jwt.sign(
-      { userId: user._id, role: user.role },
-      process.env.JWT_SECRET,
-      { expiresIn: '1h' }
-    );
-    console.log('User logged in, token generated successfully');
-    res.json({ token });
-  } else {
-    res.status(401).json({ error: 'Invalid credentials' });
-  }
-});
-
-// Logout route (handled on client)
-app.post('/logout', (req, res) => {
-  console.log('Logout route hit');
-  res.json({ message: 'Logged out successfully' });
-});
 
 // Protected character routes
 app.use('/api/characters', authenticateToken, characterRoutes);
@@ -126,8 +60,4 @@ app.get('/', (req, res) => {
   res.send('🌌 Welcome to Star Wars Character Database CRUD API server...🚀');
 });
 
-// Start server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`🚀 Server running on port ${PORT}`);
-});
+export default app;
