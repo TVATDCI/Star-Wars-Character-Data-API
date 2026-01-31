24feb28da742f297cff057583b515e0ee8501283
TVATDCI <tvatdci@gmail.com>
2026-01-31 01:17:09 +0100

    new routes

---
+++ backend/routes/userRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import { getUserProfile, updateUserProfile } from '../controllers/userController.js';
+import authenticateToken from '../middleware/authMiddleware.js';
+import { validateUserProfile } from '../middleware/validateUserProfile.js';
+
+const router = express.Router();
+
+router.route('/profile')
+  .get(authenticateToken, getUserProfile)
+  .patch(authenticateToken, validateUserProfile, updateUserProfile);
+
+export default router;
