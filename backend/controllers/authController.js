6b99e2687e1833510d6d7ab95b5f9c53525dfb00
TVATDCI <tvatdci@gmail.com>
2026-01-31 01:28:44 +0100

    update; Instead of a direct password find, now   find the user by email, then use user.comparePassword   for bcrypt authentication. standard

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -45,9 +45,9 @@ const loginUser = async (req, res, next) => {
   const { email, password } = req.body;
 
   try {
-    const user = await User.findOne({ email, password }); // Temporary until bcrypt is implemented
+    const user = await User.findOne({ email });
 
-    if (user) {
+    if (user && (await user.comparePassword(password))) {
       const token = jwt.sign(
         { userId: user._id, role: user.role },
         process.env.JWT_SECRET,
