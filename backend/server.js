c740f52f855db9a76fc748996eb85a3ddc0cfa89
TVATDCI <tvatdci@gmail.com>
2026-02-01 03:13:22 +0100

    attach gracefulShutdown

--- backend/server.js
+++ backend/server.js
@@ -3,14 +3,14 @@ import { fileURLToPath } from 'url';
 import dotenv from 'dotenv';
 import connectDB from './config/db.js';
 import app from './app.js';
+import { gracefulShutdown } from './utils/gracefulShutdown.js';
 
 // 1. Environment Configuration
-// We use the robust logic we discussed to find the .env file in root or backend
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const env = process.env.NODE_ENV || 'development';
 
-// Try loading specific env file first, fallback to standard .env
+// Implement fallback logic by trying  .env file in root or backend
 dotenv.config({ path: path.resolve(__dirname, `../.env.${env}`) });
 dotenv.config({ path: path.resolve(__dirname, '../.env') });
 
@@ -19,10 +19,11 @@ connectDB();
 
 // 3. Start Server
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`
-  🚀 Server running in ${env} mode on port ${PORT}
-  👉 http://localhost:${PORT}
+   Server running in ${env} mode on port ${PORT}
+  🌐  http://localhost:${PORT}
   `);
 });
+// 4. Graceful Shutdown
+gracefulShutdown(server);
