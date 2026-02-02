21398d64f3d47c1fa7acff2fcddf9700a090407a
TVATDCI <tvatdci@gmail.com>
2026-02-02 03:32:36 +0100

    update v1 at point

--- frontend/src/apiConfig.js
+++ frontend/src/apiConfig.js
@@ -2,4 +2,4 @@
 // Use the full production URL. Use in dev '/api' to trigger the Vite Proxy in vite.config.js
 export const API_BASE_URL = import.meta.env.PROD
   ? 'https://star-wars-character-data-api.vercel.app/api/v1'
-  : '/api';
+  : '/api/v1';
