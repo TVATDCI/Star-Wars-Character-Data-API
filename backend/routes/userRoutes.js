72cb5601acdbb482e799ad1c0b48b6ef80476998
TVATDCI <tvatdci@gmail.com>
2026-02-02 03:31:34 +0100

    update router

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,15 @@
 import express from 'express';
-import { getUserProfile, updateUserProfile } from '../controllers/userController.js';
+import {
+  getUserProfile,
+  updateUserProfile,
+} from '../controllers/userController.js';
 import authenticateToken from '../middleware/authMiddleware.js';
 import { validateUserProfile } from '../middleware/validateUserProfile.js';
 
 const router = express.Router();
 
-router.route('/profile')
+router
+  .route('/profile')
   .get(authenticateToken, getUserProfile)
   .patch(authenticateToken, validateUserProfile, updateUserProfile);
 
