9090f066b6d10ddf2de685115d540e066f253a71
TVATDCI <tvatdci@gmail.com>
2026-02-02 03:27:58 +0100

    debug

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -1,32 +1,33 @@
-import jwt from "jsonwebtoken";
-import User from "../models/userModel.js";
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel.js';
 
 //#NOTE No need to call dotenv.config() here because it's already loaded in app.js
 
 const authenticateToken = async (req, res, next) => {
-  const authHeader = req.headers["authorization"];
-  console.log("Authorization Header:", authHeader);
+  const authHeader = req.headers['authorization'];
+  console.log('Authorization Header:', authHeader);
 
-  if (!authHeader?.startsWith("Bearer ")) {
+  if (!authHeader?.startsWith('Bearer ')) {
     res.status(401);
-    throw new Error("No token provided");
+    throw new Error('No token provided');
   }
 
-  const token = authHeader.split(" ")[1];
+  const token = authHeader.split(' ')[1];
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    console.log("Decoded Token:", decoded);
+    console.log('Decoded Token:', decoded);
 
-    const user = await User.findById(decoded.userId).select("-password");
-    console.log("User Object:", user);
+    const user = await User.findById(decoded.userId).select('-password');
+    console.log('User Object:', user);
 
     if (!user) {
       res.status(401);
-      throw new Error("User not found");
+      throw new Error('User not found');
     }
 
     req.user = user;
+    console.log('Authenticated User:', req.user);
     next();
   } catch (err) {
     next(err); // Pass the error to the centralized error handler
