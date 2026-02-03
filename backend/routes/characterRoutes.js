db6d2eb2c3c7ba90baf139bb44cd0d2ba691eb32
TNTHNGVDYNND <tuanthong.vaidyanond@gmail.com>
2026-02-03 23:39:37 +0100

    remove auth token for the piblic viewing in frontend

--- backend/routes/characterRoutes.js
+++ backend/routes/characterRoutes.js
@@ -1,27 +1,27 @@
 // Initialize express router to be used in app.js
 // This is the route file for admins only!
-import express from "express";
-import authenticateToken from "../middleware/authMiddleware.js";
+import express from 'express';
+import authenticateToken from '../middleware/authMiddleware.js';
 import {
   getCharacters,
   getCharacterById,
   createCharacter,
   updateCharacter,
   deleteCharacter,
-} from "../controllers/characterController.js";
+} from '../controllers/characterController.js';
 // Import the requiredAdmin role module
-import requireAdmin from "../middleware/requireAdmin.js";
+import requireAdmin from '../middleware/requireAdmin.js';
 
 const router = express.Router();
 
 router
-  .route("/")
-  .get(authenticateToken, getCharacters)
+  .route('/')
+  .get(getCharacters)
   .post(authenticateToken, requireAdmin, createCharacter);
 
 router
-  .route("/:id")
-  .get(authenticateToken, getCharacterById)
+  .route('/:id')
+  .get(getCharacterById)
   .put(authenticateToken, requireAdmin, updateCharacter) // Only admins can update characters
   .delete(authenticateToken, requireAdmin, deleteCharacter); // Only admins can delete characters
 
